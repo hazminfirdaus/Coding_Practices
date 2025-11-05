d4ee54f2cba0369adf3944fdd74c2f75fd7e5728
Hazmin Firdaus <cmhazminfirdaus@gmail.com>
2025-11-05 01:38:23 +0100

    Update

--- C#/CsharpProjects/TestProject/Program.cs
+++ C#/CsharpProjects/TestProject/Program.cs
@@ -1,13 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Random dice = new Random();
 
-int num = 5;
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
 
-Console.WriteLine(num);
+roll1 = 1;
+roll2 = 5;
+roll3 = 1;
 
-Console.WriteLine("Rolling a dice...");
+int total = roll1 + roll2 + roll3;
 
-Random dice = new(); // C# 9.0 target-typed new expression
-// Random dice = new Random(); // Traditional way
-int roll = dice.Next();
-Console.WriteLine(roll); // 1-6
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total!");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;
+    }
+    
+    Console.WriteLine($"Your total including the bonus: {total}");
+}
+
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop!");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten.");
+}
--- C#/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ C#/CsharpProjects/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c66a1787788b08a04bd90463c5bf95bff328f9c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
